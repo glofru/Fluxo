79894abab0e5b6d0a2c82b41661e3c3763ec5780
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2025-05-31 01:48:32 -0700

    Initial Commit

---
+++ Fluxo/FluxoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxoApp.swift
+//  Fluxo
+//
+//  Created by Gianluca Lofrumento on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct FluxoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
